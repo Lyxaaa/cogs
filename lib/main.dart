4d3a02dce171d05a6f83d35fe53d9cd012a88a8a
Oscar Mahon <oscarmahon@hotmail.com>
2021-09-14 09:58:18 +1000

    Decided against firebase, literally breaks everything. started some basic structuring.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:distraction_destruction/screens/wrapper.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:developer' as dev;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
+  dev.log("Main", name: 'main');
   runApp(const MyApp());
 }
 
@@ -20,83 +20,7 @@ class MyApp extends StatelessWidget {
         //Remember to take advantage of hot reload
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'DISTRACTION DESTRUCTION'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // config for the state
-  // holds the values (title below) provided by parent (App widget above)
-  // used by build method of the State
-  // Fields in a Widget subclass are always marked "final"
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // tells Flutter framework that something has changed in this State,
-      // which causes it to rerun the build method below
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // rerunning build methods is extremely fast
-    // just rebuild anything that needs updating rather than
-    // individually changing instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        //Since this was called by _MyHomePageState, which was created
-        // in MyHomePage, we can access all of the states variables through
-        // widget.#{}
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Wrapper(),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,13 @@
 import 'package:distraction_destruction/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    dev.log("Init Wrapper", name: 'screens.wrapper');
     //Return Home/Auth widget
     return Home();
   }
