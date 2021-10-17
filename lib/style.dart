df0dbf65cce52da027b15bc4b52a33f18374fae9
Jack Emerson <mail@jackemerson.me>
2021-10-17 16:18:53 +1000

    Session stuff.

---
+++ lib/style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+// Theming generator https://flutter-theme-editor.rob-b.co.uk/#/
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  visualDensity: VisualDensity.standard,
+  primarySwatch: Colors.blue,
+);
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  visualDensity: VisualDensity.standard,
+  primarySwatch: Colors.blue,
+);
