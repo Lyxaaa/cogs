4a9f58d93f41a867271a494ce0ff63140512aaac
Oscar Mahon <oscarmahon@hotmail.com>
2021-10-19 19:09:49 +1000

    fixed profile pics occasionally not showing

--- lib/templates/profile_pic.dart
+++ lib/templates/profile_pic.dart
@@ -1,24 +1,30 @@
 import 'package:distraction_destruction/services/database.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePic extends StatelessWidget {
+class ProfilePic extends StatefulWidget {
   final VoidCallback onTap;
 
   const ProfilePic({required this.onTap});
 
   @override
+  _ProfilePicState createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+  @override
   Widget build(BuildContext context) {
+    DatabaseService database = DatabaseService();
     return GestureDetector(
-        onTap: onTap,
-        child: DatabaseService().currentProfilePicUrl == null
+        onTap: widget.onTap,
+        child: database.currentProfilePicUrl == null
             ? const CircleAvatar(
           radius: 50.0,
           child: Icon(Icons.photo_camera),
         )
-        : CircleAvatar(
-      radius: 50.0,
-      backgroundImage: NetworkImage(DatabaseService().currentProfilePicUrl!),
-    )
+            : CircleAvatar(
+          radius: 50.0,
+          backgroundImage: NetworkImage(database.currentProfilePicUrl!),
+        )
     );
   }
 }
